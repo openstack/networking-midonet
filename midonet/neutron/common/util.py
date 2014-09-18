midokura/python-neutron-plugin-midonet

Add skeleton of a decorator for API handler mixins
This patch adds a decorator function that generates required methods for
API handler mixins based on the given list of strings represent required
handlers.

Change-Id: I8da516c849d1216201af9e753ab9fd1fe6ce9ddb
Signed-off-by: Taku Fukushima <tfukushima@midokura.com>

midonet/neutron/common/util.py
@@ -14,6 +14,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import re
 import webob
 from webob import exc as w_exc
 
@@ -141,3 +142,58 @@ def midonet_extension(cls):
             setattr(cls, method.__name__, classmethod(method))
 
     return cls
+
+
+def generate_methods(methods):
+    """Decorator for classes that represents which HTTP methods are required by
+    the classes.
+    """
+    ALLOWED_METHODS = (base.Controller.LIST,
+                       base.Controller.SHOW,
+                       base.Controller.CREATE,
+                       base.Controller.UPDATE,
+                       base.Controller.DELETE)
+
+    @handle_api_error
+    def create_resource(self, context, id, resource):
+        pass
+
+    @handle_api_error
+    def update_resource(self, context, id, resource):
+        pass
+
+    @handle_api_error
+    def get_resource(self, context, id, fields):
+        pass
+
+    @handle_api_error
+    def get_resources(self, context, filters, fields):
+        pass
+
+    @handle_api_error
+    def delete_resource(self, context, id):
+        pass
+
+    AVAILABLE_METHODS = {base.Controller.LIST: get_resources,
+                         base.Controller.SHOW: get_resource,
+                         base.Controller.CREATE: create_resource,
+                         base.Controller.UPDATE: update_resource,
+                         base.Controller.DELETE: delete_resource}
+
+    required_methods = [method for method in methods
+                        if method in ALLOWED_METHODS]
+
+    def wrapper(cls):
+        # Use the first capitalzed word as an alias.
+        [capitalized_resource] = re.findall('^[A-Z][a-z0-9]*', cls.__name__)
+        alias = capitalized_resource.lower()
+        setattr(cls, 'ALIAS', alias)
+        for method in required_methods:
+            method_name = method + '_' + alias
+            try:
+                getattr(cls, method_name)
+            except AttributeError:
+                setattr(cls, method_name, AVAILABLE_METHODS[method])
+        return cls
+
+    return wrapper

openstack/networking-midonet

midonet/neutron/common/util.py
@@ -14,11 +14,12 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import re
 from webob import exc as w_exc
 
 from midonetclient import exc
 
-
+from neutron.api.v2 import base
 from neutron.common import exceptions as n_exc
 from neutron.openstack.common import log as logging
 
@@ -43,3 +44,58 @@ class MidonetApiException(n_exc.NeutronException):
 
 class MidonetPluginException(n_exc.NeutronException):
     message = _("%(msg)s")
+
+
+def generate_methods(methods):
+    """Decorator for classes that represents which methods are required by the
+    classes.
+
+    :param methods: The list of methods to be generated automatically. They
+                    must be some or one of 'list', 'show', 'create', 'update'
+                    and 'delete'.
+    """
+
+    @handle_api_error
+    def create_resource(self, context, id, resource):
+        pass
+
+    @handle_api_error
+    def update_resource(self, context, id, resource):
+        pass
+
+    @handle_api_error
+    def get_resource(self, context, id, fields):
+        pass
+
+    @handle_api_error
+    def get_resources(self, context, filters, fields):
+        pass
+
+    @handle_api_error
+    def delete_resource(self, context, id):
+        pass
+
+    AVAILABLE_METHOD_MAP = {base.Controller.LIST: get_resources,
+                            base.Controller.SHOW: get_resource,
+                            base.Controller.CREATE: create_resource,
+                            base.Controller.UPDATE: update_resource,
+                            base.Controller.DELETE: delete_resource}
+    ALLOWED_METHODS = AVAILABLE_METHOD_MAP.keys()
+
+    required_methods = [method for method in methods
+                        if method in ALLOWED_METHODS]
+
+    def wrapper(cls):
+        # Use the first capitalzed word as an alias.
+        [capitalized_resource] = re.findall('^[A-Z][a-z0-9]*', cls.__name__)
+        alias = capitalized_resource.lower()
+        setattr(cls, 'ALIAS', alias)
+        for method in required_methods:
+            method_name = method + '_' + alias
+            try:
+                getattr(cls, method_name)
+            except AttributeError:
+                setattr(cls, method_name, AVAILABLE_METHOD_MAP[method])
+        return cls
+
+    return wrapper