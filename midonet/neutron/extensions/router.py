tfukushima/python-neutron-plugin-midonet

Add API handlers for routers
This patch adds the API handlers for routers to MidonetPluginV2
with a unit test for the HTTP POST.

Change-Id: I1f8e0ccef242b02b92be9c06e08c1ad82db247ba
Signed-off-by: Taku Fukushima <tfukushima@midokura.com>

midonet/neutron/extensions/router.py
@@ -27,7 +27,7 @@ ROUTERS = '%ss' % ROUTER
 
 RESOURCE_ATTRIBUTE_MAP = {
     ROUTERS: {
-        'id': {'allow_post': True, 'allow_put': False,
+        'id': {'allow_post': False, 'allow_put': False,
                'validate': {'type:uuid': None},
                'is_visible': True, 'default': None},
         'inbound_filter_id': {'allow_post': True, 'allow_put': True,
@@ -38,15 +38,16 @@ RESOURCE_ATTRIBUTE_MAP = {
                              'is_visible': True, 'default': None},
         'name': {'allow_post': True, 'allow_put': True,
                  'validate': {'type:string': None},
-                 'is_visible': True, 'default': None},
+                 'is_visible': True, 'default': ''},
         'outbound_filter_id': {'allow_post': True, 'allow_put': True,
                                'validate': {'type:uuid_or_none': None},
                                'is_visible': True, 'default': None},
         'tenant_id': {'allow_post': True, 'allow_put': True,
-                      'validate': {'type:string': None},
+                      'validate': {'type:uuid': None},
                       'is_visible': True, 'default': None},
         'vxlan_port_id': {'allow_post': False, 'allow_put': False,
-                          'is_visible': True}
+                          'validate': {'type:uuid_or_none': None},
+                          'is_visible': True, 'default': None}
     }
 }
 
@@ -102,33 +103,24 @@ class Router(extensions.ExtensionDescriptor):
 
 
 @six.add_metaclass(abc.ABCMeta)
-class MidonetRouterPluginBase(object):
-
-    def get_plugin_name(self):
-        return "router plugin"
-
-    def get_plugin_type(self):
-        return "router"
-
-    def get_plugin_description(self):
-        return "Router extension base plugin"
+class RouterPluginBase(object):
 
     @abc.abstractmethod
-    def create_router(self, context, id, router):
+    def get_midonet_routers(self, context, filters=None, fields=None):
         pass
 
     @abc.abstractmethod
-    def update_router(self, context, id, router):
+    def get_midonet_router(self, context, i, fields=None):
         pass
 
     @abc.abstractmethod
-    def get_router(self, context, router, fields=None):
+    def create_midonet_router(self, context, router):
         pass
 
     @abc.abstractmethod
-    def delete_router(self, context, id):
+    def update_midonet_router(self, context, id, router):
         pass
 
     @abc.abstractmethod
-    def get_routers(self, context, filters=None, fields=None):
+    def delete_midonet_router(self, context, id):
         pass

midonet/neutron/tests/unit/test_extension_router.py
@@ -11,6 +11,8 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
+
+import copy
 import mock
 from webob import exc
 
@@ -31,8 +33,8 @@ class RouterExtensionTestCase(test_api_v2_extension.ExtensionTestCase):
         super(RouterExtensionTestCase, self).setUp()
         plural_mappings = {'midonet_router': 'midonet_routers'}
         self._setUpExtension(
-            'midonet.neutron.extensions.router.MidonetRouterPluginBase',
-            router.ROUTER, router.RESOURCE_ATTRIBUTE_MAP,
+            'midonet.neutron.extensions.router.RouterPluginBase',
+            None, router.RESOURCE_ATTRIBUTE_MAP,
             router.Router, '', plural_mappings=plural_mappings)
 
     def test_router_list(self):
@@ -43,14 +45,12 @@ class RouterExtensionTestCase(test_api_v2_extension.ExtensionTestCase):
                          'tenant_id': _uuid()}]
 
         instance = self.plugin.return_value
-        instance.get_routers.return_value = return_value
+        instance.get_midonet_routers.return_value = return_value
 
         res = self.api.get(_get_path('routers', fmt=self.fmt))
         self.assertEqual(exc.HTTPOk.code, res.status_int)
-
-        instance.get_routers.assert_called_once_with(
+        instance.get_midonet_routers.assert_called_once_with(
             mock.ANY, fields=mock.ANY, filters=mock.ANY)
-
         res = self.deserialize(res)
         self.assertIn('routers', res)
         self.assertEqual(1, len(res['routers']))
@@ -64,17 +64,36 @@ class RouterExtensionTestCase(test_api_v2_extension.ExtensionTestCase):
                         'tenant_id': _uuid()}
 
         instance = self.plugin.return_value
-        instance.get_router.return_value = return_value
+        instance.get_midonet_router.return_value = return_value
 
         res = self.api.get(_get_path('routers/%s' % router_id,
                            fmt=self.fmt))
         self.assertEqual(exc.HTTPOk.code, res.status_int)
-
-        instance.get_router.assert_called_once_with(
+        instance.get_midonet_router.assert_called_once_with(
             mock.ANY, unicode(router_id), fields=mock.ANY)
-
         res = self.deserialize(res)
         self.assertIn('router', res)
+
+    def test_router_create(self):
+        router_id = _uuid()
+        data = {'midonet_router': {'name': 'BLAH BLAH',
+                                   'inbound_filter_id': None,
+                                   'outbound_filter_id': None,
+                                   'tenant_id': _uuid()}}
+        return_value = copy.deepcopy(data['midonet_router'])
+        return_value.update({'id': router_id})
+        instance = self.plugin.return_value
+        instance.create_midonet_router.return_value = return_value
+
+        res = self.api.post(_get_path('midonet_routers', fmt=self.fmt),
+                            self.serialize(data),
+                            content_type='application/%s' % self.fmt)
+        self.assertEqual(exc.HTTPCreated.code, res.status_int)
+        instance.create_midonet_router.assert_called_once_with(
+            mock.ANY, midonet_router=data)
+        res = self.deserialize(data)
+        self.assertIn('midonet_router', res)
+        self.assertEqual(res['midonet_router'], return_value)
 
     def test_router_update(self):
         router_id = _uuid()
@@ -83,30 +102,26 @@ class RouterExtensionTestCase(test_api_v2_extension.ExtensionTestCase):
                         'inbound_filter_id': _uuid(),
                         'outbound_filter_id': _uuid(),
                         'tenant_id': _uuid()}
-
         update_data = {'router': {'name': 'ladeeda'}}
-
         instance = self.plugin.return_value
-        instance.update_router.return_value = return_value
+        instance.update_midonet_router.return_value = return_value
 
         res = self.api.put(_get_path('routers', id=router_id,
                            fmt=self.fmt), self.serialize(update_data))
 
-        instance.update_router.assert_called_once_with(
+        instance.update_midonet_router.assert_called_once_with(
             mock.ANY, router_id, router=update_data)
         self.assertEqual(exc.HTTPOk.code, res.status_int)
 
     def test_router_delete(self):
         router_id = _uuid()
-
         instance = self.plugin.return_value
 
         res = self.api.delete(_get_path('routers', id=router_id))
-
-        instance.delete_router.assert_called_once_with(mock.ANY, router_id)
+        instance.delete_midonet_router.assert_called_once_with(
+            mock.ANY, router_id)
         self.assertEqual(exc.HTTPNoContent.code, res.status_int)
 
 
 class RouterExtensionTestCaseXml(RouterExtensionTestCase):
-
     fmt = "xml"

openstack/networking-midonet

midonet/neutron/extensions/router.py
@@ -22,12 +22,12 @@ from neutron.api import extensions
 from neutron.api.v2 import base
 from neutron import manager
 
-ROUTER = 'router'
+ROUTER = 'midonet_router'
 ROUTERS = '%ss' % ROUTER
 
 RESOURCE_ATTRIBUTE_MAP = {
     ROUTERS: {
-        'id': {'allow_post': True, 'allow_put': False,
+        'id': {'allow_post': False, 'allow_put': False,
                'validate': {'type:uuid': None},
                'is_visible': True, 'default': None},
         'inbound_filter_id': {'allow_post': True, 'allow_put': True,
@@ -38,15 +38,16 @@ RESOURCE_ATTRIBUTE_MAP = {
                              'is_visible': True, 'default': None},
         'name': {'allow_post': True, 'allow_put': True,
                  'validate': {'type:string': None},
-                 'is_visible': True, 'default': None},
+                 'is_visible': True, 'default': ''},
         'outbound_filter_id': {'allow_post': True, 'allow_put': True,
                                'validate': {'type:uuid_or_none': None},
                                'is_visible': True, 'default': None},
         'tenant_id': {'allow_post': True, 'allow_put': True,
-                      'validate': {'type:string': None},
+                      'validate': {'type:uuid': None},
                       'is_visible': True, 'default': None},
         'vxlan_port_id': {'allow_post': False, 'allow_put': False,
-                          'is_visible': True}
+                          'validate': {'type:uuid_or_none': None},
+                          'is_visible': True, 'default': None}
     }
 }
 
@@ -102,33 +103,24 @@ class Router(extensions.ExtensionDescriptor):
 
 
 @six.add_metaclass(abc.ABCMeta)
-class MidonetRouterPluginBase(object):
-
-    def get_plugin_name(self):
-        return "router plugin"
-
-    def get_plugin_type(self):
-        return "router"
-
-    def get_plugin_description(self):
-        return "Router extension base plugin"
+class RouterPluginBase(object):
 
     @abc.abstractmethod
-    def create_router(self, context, id, router):
+    def get_midonet_routers(self, context, filters=None, fields=None):
         pass
 
     @abc.abstractmethod
-    def update_router(self, context, id, router):
+    def get_midonet_router(self, context, id, fields=None):
         pass
 
     @abc.abstractmethod
-    def get_router(self, context, router, fields=None):
+    def create_midonet_router(self, context, midonet_router):
         pass
 
     @abc.abstractmethod
-    def delete_router(self, context, id):
+    def update_midonet_router(self, context, id, midonet_router):
         pass
 
     @abc.abstractmethod
-    def get_routers(self, context, filters=None, fields=None):
+    def delete_midonet_router(self, context, id):
         pass

midonet/neutron/tests/unit/test_extension_router.py
@@ -11,6 +11,8 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
+
+import copy
 import mock
 from webob import exc
 
@@ -31,8 +33,8 @@ class RouterExtensionTestCase(test_api_v2_extension.ExtensionTestCase):
         super(RouterExtensionTestCase, self).setUp()
         plural_mappings = {'midonet_router': 'midonet_routers'}
         self._setUpExtension(
-            'midonet.neutron.extensions.router.MidonetRouterPluginBase',
-            router.ROUTER, router.RESOURCE_ATTRIBUTE_MAP,
+            'midonet.neutron.extensions.router.RouterPluginBase',
+            None, router.RESOURCE_ATTRIBUTE_MAP,
             router.Router, '', plural_mappings=plural_mappings)
 
     def test_router_list(self):
@@ -43,17 +45,15 @@ class RouterExtensionTestCase(test_api_v2_extension.ExtensionTestCase):
                          'tenant_id': _uuid()}]
 
         instance = self.plugin.return_value
-        instance.get_routers.return_value = return_value
+        instance.get_midonet_routers.return_value = return_value
 
-        res = self.api.get(_get_path('routers', fmt=self.fmt))
+        res = self.api.get(_get_path('midonet_routers', fmt=self.fmt))
         self.assertEqual(exc.HTTPOk.code, res.status_int)
-
-        instance.get_routers.assert_called_once_with(
+        instance.get_midonet_routers.assert_called_once_with(
             mock.ANY, fields=mock.ANY, filters=mock.ANY)
-
         res = self.deserialize(res)
-        self.assertIn('routers', res)
-        self.assertEqual(1, len(res['routers']))
+        self.assertIn('midonet_routers', res)
+        self.assertEqual(1, len(res['midonet_routers']))
 
     def test_router_show(self):
         router_id = _uuid()
@@ -64,17 +64,37 @@ class RouterExtensionTestCase(test_api_v2_extension.ExtensionTestCase):
                         'tenant_id': _uuid()}
 
         instance = self.plugin.return_value
-        instance.get_router.return_value = return_value
+        instance.get_midonet_router.return_value = return_value
 
-        res = self.api.get(_get_path('routers/%s' % router_id,
+        res = self.api.get(_get_path('midonet_routers/%s' % router_id,
                            fmt=self.fmt))
         self.assertEqual(exc.HTTPOk.code, res.status_int)
-
-        instance.get_router.assert_called_once_with(
+        instance.get_midonet_router.assert_called_once_with(
             mock.ANY, unicode(router_id), fields=mock.ANY)
-
         res = self.deserialize(res)
-        self.assertIn('router', res)
+        self.assertIn('midonet_router', res)
+
+    def test_router_create(self):
+        router_id = _uuid()
+        data = {'midonet_router': {'name': 'BLAH BLAH',
+                                   'inbound_filter_id': None,
+                                   'outbound_filter_id': None,
+                                   'load_balancer_id': None,
+                                   'tenant_id': _uuid()}}
+        return_value = copy.deepcopy(data['midonet_router'])
+        return_value.update({'id': router_id})
+        instance = self.plugin.return_value
+        instance.create_midonet_router.return_value = return_value
+
+        res = self.api.post(_get_path('midonet_routers', fmt=self.fmt),
+                            self.serialize(data),
+                            content_type='application/%s' % self.fmt)
+        self.assertEqual(exc.HTTPCreated.code, res.status_int)
+        instance.create_midonet_router.assert_called_once_with(
+            mock.ANY, midonet_router=data)
+        res = self.deserialize(res)
+        self.assertIn('midonet_router', res)
+        self.assertEqual(res['midonet_router'], return_value)
 
     def test_router_update(self):
         router_id = _uuid()
@@ -83,30 +103,27 @@ class RouterExtensionTestCase(test_api_v2_extension.ExtensionTestCase):
                         'inbound_filter_id': _uuid(),
                         'outbound_filter_id': _uuid(),
                         'tenant_id': _uuid()}
-
-        update_data = {'router': {'name': 'ladeeda'}}
-
+        update_data = {'midonet_router': {'name': 'ladeeda'}}
         instance = self.plugin.return_value
-        instance.update_router.return_value = return_value
+        instance.update_midonet_router.return_value = return_value
 
-        res = self.api.put(_get_path('routers', id=router_id,
-                           fmt=self.fmt), self.serialize(update_data))
-
-        instance.update_router.assert_called_once_with(
-            mock.ANY, router_id, router=update_data)
+        res = self.api.put(
+            _get_path('midonet_routers', id=router_id,
+                      fmt=self.fmt),
+            self.serialize(update_data))
+        instance.update_midonet_router.assert_called_once_with(
+            mock.ANY, router_id, midonet_router=update_data)
         self.assertEqual(exc.HTTPOk.code, res.status_int)
 
     def test_router_delete(self):
         router_id = _uuid()
-
         instance = self.plugin.return_value
 
-        res = self.api.delete(_get_path('routers', id=router_id))
-
-        instance.delete_router.assert_called_once_with(mock.ANY, router_id)
+        res = self.api.delete(_get_path('midonet_routers', id=router_id))
+        instance.delete_midonet_router.assert_called_once_with(
+            mock.ANY, router_id)
         self.assertEqual(exc.HTTPNoContent.code, res.status_int)
 
 
 class RouterExtensionTestCaseXml(RouterExtensionTestCase):
-
     fmt = "xml"