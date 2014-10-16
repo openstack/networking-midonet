tfukushima/python-neutron-plugin-midonet

Add API handlers for DHCP subnets and their hosts
This patch adds the API handlers for DHCP subnets and DHCP subnet
hosts.

This patch adds comprehensive unit tests for the DHCP subnets.

Change-Id: If93d2164e48f610a9c6d5e1109cd9c75a51b14ce
Signed-off-by: Taku Fukushima <tfukushima@midokura.com>

midonet/neutron/extensions/subnet.py
@@ -19,56 +19,70 @@ import abc
 import six
 
 from neutron.api import extensions
+from neutron.api.v2 import attributes as attr
 from neutron.api.v2 import base
 from neutron import manager
 from neutron.openstack.common import log as logging
 
 LOG = logging.getLogger(__name__)
 
-DHCP = 'dhcp_host'
-DHCPS = '%ss' % DHCP
+DHCP_HOST = 'dhcp_host'
+DHCP_HOSTS = '%ss' % DHCP_HOST
 
 SUBNET = 'midonet_subnet'
 SUBNETS = '%ss' % SUBNET
 
 
+# Monkey patches to add validations.
+def _validate_non_negative_or_none(data, valid_values=None):
+    if data is not None:
+        attr._validate_non_negative_or_none(data, valid_values)
+
+
+def _validate_range_or_none(data, valid_values=None):
+    if data is not None:
+        attr._validate_range(data, valid_values)
+
+
+attr.validators['type:non_negative_or_none'] = _validate_non_negative_or_none
+attr.validators['type:range_or_none'] = _validate_range_or_none
+
+
 RESOURCE_ATTRIBUTE_MAP = {
     SUBNETS: {
         'default_gateway': {'allow_post': True, 'allow_put': False,
-                            'default': None,
                             'validate': {'type:ip_address_or_none': None},
-                            'is_visible': True},
+                            'is_visible': True, 'default': None},
+        'enabled': {'allow_post': True, 'allow_put': False,
+                    'validate': {'type:boolean': None},
+                    'is_visible': True, 'default': True},
         'server_addr': {'allow_post': True, 'allow_put': False,
-                        'default': None,
                         'validate': {'type:ip_address_or_none': None},
-                        'is_visible': True},
+                        'is_visible': True, 'default': None},
         'dns_server_addrs': {'allow_post': True, 'allow_put': False,
-                             'default': None,
-                             'is_visible': True},
+                             'validate': {'type:ip_address_or_none': None},
+                             'is_visible': True, 'default': None},
         'subnet_prefix': {'allow_post': True, 'allow_put': False,
-                          'default': None,
                           'validate': {'type:ip_address_or_none': None},
-                          'is_visible': True},
+                          'is_visible': True, 'default': None},
         'subnet_length': {'allow_post': True, 'allow_put': False,
-                          'default': None,
-                          'validate': {'type:range': [0, 32]},
-                          'is_visible': True},
+                          'validate': {'type:range_or_none': [0, 32]},
+                          'is_visible': True, 'default': None},
         'interface_mtu': {'allow_post': True, 'allow_put': False,
-                          'default': None,
-                          'validate': {'type:non_negative': None},
-                          'is_visible': True},
+                          'validate': {'type:non_negative_or_none': None},
+                          'is_visible': True, 'default': None},
         'tenant_id': {'allow_post': True, 'allow_put': False,
                       'required_by_policy': True,
-                      'validate': {'type:string': None},
+                      'validate': {'type:uuid': None},
                       'is_visible': True},
     },
-    DHCPS: {
+    DHCP_HOSTS: {
         'ip_address': {'allow_post': True, 'allow_put': True,
                        'validate': {'type:ip_address': None},
-                       'is_visible': True},
+                       'is_visible': True, 'required_by_policy': True},
         'mac_address': {'allow_post': True, 'allow_put': True,
                         'validate': {'type:mac_address': None},
-                        'is_visible': True},
+                        'is_visible': True, 'required_by_policy': True},
         'tenant_id': {'allow_post': True, 'allow_put': False,
                       'required_by_policy': True,
                       'validate': {'type:string': None},
@@ -108,32 +122,24 @@ class Subnet(extensions.ExtensionDescriptor):
         exts = []
         plugin = manager.NeutronManager.get_plugin()
 
-        # hosts
-        parent = dict(member_name="midonet_subnet",
-                      collection_name="midonet_subnets")
-        collection_name = DHCPS
-        params = RESOURCE_ATTRIBUTE_MAP.get(collection_name, dict())
-        controller_host = base.create_resource(collection_name,
-                                               DHCP,
-                                               plugin,
-                                               params,
-                                               parent=parent,
-                                               allow_bulk=True)
-
-        ex = extensions.ResourceExtension(collection_name,
-                                          controller_host,
-                                          parent=parent)
-        exts.append(ex)
-
         # subnets
         collection_name = SUBNETS
         params = RESOURCE_ATTRIBUTE_MAP.get(collection_name, dict())
-        controller_host = base.create_resource(collection_name, SUBNET,
-                                               plugin, params,
-                                               allow_bulk=True)
+        subnet_controller = base.create_resource(
+            collection_name, SUBNET, plugin, params, allow_bulk=True)
+        ex = extensions.ResourceExtension(collection_name, subnet_controller)
+        exts.append(ex)
 
-        ex = extensions.ResourceExtension(collection_name, controller_host)
-
+        # hosts
+        parent = dict(member_name=SUBNET,
+                      collection_name=SUBNETS)
+        collection_name = DHCP_HOSTS
+        params = RESOURCE_ATTRIBUTE_MAP.get(collection_name, dict())
+        host_controller = base.create_resource(
+            collection_name, DHCP_HOST, plugin, params,
+            parent=parent, allow_bulk=True)
+        ex = extensions.ResourceExtension(
+            collection_name, host_controller, parent=parent)
         exts.append(ex)
 
         return exts
@@ -146,18 +152,31 @@ class Subnet(extensions.ExtensionDescriptor):
 
 
 @six.add_metaclass(abc.ABCMeta)
-class SubnetsPluginBase(object):
+class SubnetPluginBase(object):
 
-    __native_bulk_support = True
+    @abc.abstractmethod
+    def create_midonet_subnet(self, context, midonet_subnet):
+        pass
 
-    def get_plugin_name(self):
-        return "DHCP Host Plugin"
+    @abc.abstractmethod
+    def update_midonet_subnet(self, context, id, midonet_subnet):
+        pass
 
-    def get_plugin_type(self):
-        return "dhcp-host"
+    @abc.abstractmethod
+    def get_midonet_subnet(self, context, midonet_subnet, fields=None):
+        pass
 
-    def get_plugin_description(self):
-        return "Base plugin for DHCP Host extension"
+    @abc.abstractmethod
+    def delete_midonet_subnet(self, context, id):
+        pass
+
+    @abc.abstractmethod
+    def get_midonet_subnets(self, context, filters=None, fields=None):
+        pass
+
+
+@six.add_metaclass(abc.ABCMeta)
+class SubnetDhcpHostPluginBase(object):
 
     @abc.abstractmethod
     def get_midonet_subnet_dhcp_host(self, context, id, midonet_subnet_id,
@@ -181,24 +200,4 @@ class SubnetsPluginBase(object):
     @abc.abstractmethod
     def create_midonet_subnet_dhcp_host(self, context, midonet_subnet_id,
                                         dhcp_host):
-        pass
-
-    @abc.abstractmethod
-    def create_midonet_subnet(self, context, midonet_subnet):
-        pass
-
-    @abc.abstractmethod
-    def update_midonet_subnet(self, context, id, midonet_subnet):
-        pass
-
-    @abc.abstractmethod
-    def get_midonet_subnet(self, context, midonet_subnet, fields=None):
-        pass
-
-    @abc.abstractmethod
-    def delete_midonet_subnet(self, context, id):
-        pass
-
-    @abc.abstractmethod
-    def get_midonet_subnets(self, context, filters=None, fields=None):
         pass

openstack/networking-midonet

midonet/neutron/extensions/subnet.py
@@ -19,56 +19,70 @@ import abc
 import six
 
 from neutron.api import extensions
+from neutron.api.v2 import attributes as attr
 from neutron.api.v2 import base
 from neutron import manager
 from neutron.openstack.common import log as logging
 
 LOG = logging.getLogger(__name__)
 
-DHCP = 'dhcp_host'
-DHCPS = '%ss' % DHCP
+DHCP_HOST = 'dhcp_host'
+DHCP_HOSTS = '%ss' % DHCP_HOST
 
 SUBNET = 'midonet_subnet'
 SUBNETS = '%ss' % SUBNET
 
 
+# Monkey patches to add validations.
+def _validate_non_negative_or_none(data, valid_values=None):
+    if data is not None:
+        attr._validate_non_negative(data, valid_values)
+
+
+def _validate_range_or_none(data, valid_values=None):
+    if data is not None:
+        attr._validate_range(data, valid_values)
+
+
+attr.validators['type:non_negative_or_none'] = _validate_non_negative_or_none
+attr.validators['type:range_or_none'] = _validate_range_or_none
+
+
 RESOURCE_ATTRIBUTE_MAP = {
     SUBNETS: {
-        'default_gateway': {'allow_post': True, 'allow_put': False,
-                            'default': None,
+        'default_gateway': {'allow_post': True, 'allow_put': True,
                             'validate': {'type:ip_address_or_none': None},
-                            'is_visible': True},
-        'server_addr': {'allow_post': True, 'allow_put': False,
-                        'default': None,
+                            'is_visible': True, 'default': None},
+        'enabled': {'allow_post': True, 'allow_put': True,
+                    'validate': {'type:boolean': None},
+                    'is_visible': True, 'default': True},
+        'server_addr': {'allow_post': True, 'allow_put': True,
                         'validate': {'type:ip_address_or_none': None},
-                        'is_visible': True},
-        'dns_server_addrs': {'allow_post': True, 'allow_put': False,
-                             'default': None,
-                             'is_visible': True},
-        'subnet_prefix': {'allow_post': True, 'allow_put': False,
-                          'default': None,
+                        'is_visible': True, 'default': None},
+        'dns_server_addrs': {'allow_post': True, 'allow_put': True,
+                             'validate': {'type:ip_address_or_none': None},
+                             'is_visible': True, 'default': None},
+        'subnet_prefix': {'allow_post': True, 'allow_put': True,
                           'validate': {'type:ip_address_or_none': None},
-                          'is_visible': True},
-        'subnet_length': {'allow_post': True, 'allow_put': False,
-                          'default': None,
-                          'validate': {'type:range': [0, 32]},
-                          'is_visible': True},
-        'interface_mtu': {'allow_post': True, 'allow_put': False,
-                          'default': None,
-                          'validate': {'type:non_negative': None},
-                          'is_visible': True},
+                          'is_visible': True, 'default': None},
+        'subnet_length': {'allow_post': True, 'allow_put': True,
+                          'validate': {'type:range_or_none': [0, 32]},
+                          'is_visible': True, 'default': None},
+        'interface_mtu': {'allow_post': True, 'allow_put': True,
+                          'validate': {'type:non_negative_or_none': None},
+                          'is_visible': True, 'default': None},
         'tenant_id': {'allow_post': True, 'allow_put': False,
                       'required_by_policy': True,
-                      'validate': {'type:string': None},
+                      'validate': {'type:uuid': None},
                       'is_visible': True},
     },
-    DHCPS: {
+    DHCP_HOSTS: {
         'ip_address': {'allow_post': True, 'allow_put': True,
                        'validate': {'type:ip_address': None},
-                       'is_visible': True},
+                       'is_visible': True, 'required_by_policy': True},
         'mac_address': {'allow_post': True, 'allow_put': True,
                         'validate': {'type:mac_address': None},
-                        'is_visible': True},
+                        'is_visible': True, 'required_by_policy': True},
         'tenant_id': {'allow_post': True, 'allow_put': False,
                       'required_by_policy': True,
                       'validate': {'type:string': None},
@@ -92,7 +106,7 @@ class Subnet(extensions.ExtensionDescriptor):
 
     @classmethod
     def get_description(cls):
-        return ("Neutron subnet with midonet extensions")
+        return "Neutron subnet with midonet extensions"
 
     @classmethod
     def get_namespace(cls):
@@ -108,32 +122,24 @@ class Subnet(extensions.ExtensionDescriptor):
         exts = []
         plugin = manager.NeutronManager.get_plugin()
 
-        # hosts
-        parent = dict(member_name="midonet_subnet",
-                      collection_name="midonet_subnets")
-        collection_name = DHCPS
-        params = RESOURCE_ATTRIBUTE_MAP.get(collection_name, dict())
-        controller_host = base.create_resource(collection_name,
-                                               DHCP,
-                                               plugin,
-                                               params,
-                                               parent=parent,
-                                               allow_bulk=True)
-
-        ex = extensions.ResourceExtension(collection_name,
-                                          controller_host,
-                                          parent=parent)
-        exts.append(ex)
-
         # subnets
         collection_name = SUBNETS
         params = RESOURCE_ATTRIBUTE_MAP.get(collection_name, dict())
-        controller_host = base.create_resource(collection_name, SUBNET,
-                                               plugin, params,
-                                               allow_bulk=True)
+        subnet_controller = base.create_resource(
+            collection_name, SUBNET, plugin, params, allow_bulk=True)
+        ex = extensions.ResourceExtension(collection_name, subnet_controller)
+        exts.append(ex)
 
-        ex = extensions.ResourceExtension(collection_name, controller_host)
-
+        # hosts
+        parent = dict(member_name=SUBNET,
+                      collection_name=SUBNETS)
+        collection_name = DHCP_HOSTS
+        params = RESOURCE_ATTRIBUTE_MAP.get(collection_name, dict())
+        host_controller = base.create_resource(
+            collection_name, DHCP_HOST, plugin, params,
+            parent=parent, allow_bulk=True)
+        ex = extensions.ResourceExtension(
+            collection_name, host_controller, parent=parent)
         exts.append(ex)
 
         return exts
@@ -146,18 +152,31 @@ class Subnet(extensions.ExtensionDescriptor):
 
 
 @six.add_metaclass(abc.ABCMeta)
-class SubnetsPluginBase(object):
+class SubnetPluginBase(object):
 
-    __native_bulk_support = True
+    @abc.abstractmethod
+    def create_midonet_subnet(self, context, midonet_subnet):
+        pass
 
-    def get_plugin_name(self):
-        return "DHCP Host Plugin"
+    @abc.abstractmethod
+    def update_midonet_subnet(self, context, id, midonet_subnet):
+        pass
 
-    def get_plugin_type(self):
-        return "dhcp-host"
+    @abc.abstractmethod
+    def get_midonet_subnet(self, context, midonet_subnet, fields=None):
+        pass
 
-    def get_plugin_description(self):
-        return "Base plugin for DHCP Host extension"
+    @abc.abstractmethod
+    def delete_midonet_subnet(self, context, id):
+        pass
+
+    @abc.abstractmethod
+    def get_midonet_subnets(self, context, filters=None, fields=None):
+        pass
+
+
+@six.add_metaclass(abc.ABCMeta)
+class SubnetDhcpHostPluginBase(object):
 
     @abc.abstractmethod
     def get_midonet_subnet_dhcp_host(self, context, id, midonet_subnet_id,
@@ -181,24 +200,4 @@ class SubnetsPluginBase(object):
     @abc.abstractmethod
     def create_midonet_subnet_dhcp_host(self, context, midonet_subnet_id,
                                         dhcp_host):
-        pass
-
-    @abc.abstractmethod
-    def create_midonet_subnet(self, context, midonet_subnet):
-        pass
-
-    @abc.abstractmethod
-    def update_midonet_subnet(self, context, id, midonet_subnet):
-        pass
-
-    @abc.abstractmethod
-    def get_midonet_subnet(self, context, midonet_subnet, fields=None):
-        pass
-
-    @abc.abstractmethod
-    def delete_midonet_subnet(self, context, id):
-        pass
-
-    @abc.abstractmethod
-    def get_midonet_subnets(self, context, filters=None, fields=None):
         pass