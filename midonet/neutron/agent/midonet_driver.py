e5ebefc1445691e36491125a9284e6d30a3ebf20
Tomoe Sugihara <tomoe@midokura.com>
2014-10-29 07:44:04 +0000

    Run neutron-ns-metadata-proxy in isolated networks
    Since we make use of neutron-ns-metadata-proxy
    running in dhcp netns even with tenant router,
    spawn neutron-ns-metadata-proxy always.
    
    The other class method get_isolated_subnets
    is not used except from dhcp.Dnsmasq.get_isolated_subnets.
    So we unimplement it.
    
    Change-Id: I7a23b344f40a34b208c5e0963525a34b806740c2
    Fix: MN-3049

--- midonet/neutron/agent/midonet_driver.py
+++ midonet/neutron/agent/midonet_driver.py
@@ -52,8 +52,7 @@ class DhcpNoOpDriver(dhcp.DhcpLocalProcess):
     def spawn_process(self):
         pass
 
-    # Quick fix to catch up with a cange in upstream:
-    # https://github.com/openstack/neutron/commit/
-    #     9569b2fe58d0e836071992f545886ca985d5ace8
-    should_enable_metadata = dhcp.Dnsmasq.should_enable_metadata
-    get_isolated_subnets = dhcp.Dnsmasq.get_isolated_subnets
+    @classmethod
+    def should_enable_metadata(cls, conf, network):
+        """We need MD namespace proxy regardless of network configuration"""
+        return True
