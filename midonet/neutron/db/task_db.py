a4ada8d6978ecaf77073436b2056599942f51e64
Joe Mills <joe@midokura.com>
2015-03-16 06:57:54 +0000

    Add data sync tables to neutron db
    Adds midonet_data_state, midonet_versions, and
    removes midonet_task_state from the midonet database.
    
    Partially-Implements: kilo/data_sync
    Change-Id: Ie1332cc9ee301bf38f859fd0f9408de78c5cb53a
    Signed-off-by: Joe Mills <joe@midokura.com>

--- midonet/neutron/db/task_db.py
+++ midonet/neutron/db/task_db.py
@@ -48,16 +48,22 @@ AGENT_MEMBERSHIP = "AGENTMEMBERSHIP"
 OP_IMPORT = 'IMPORT'
 OP_FLUSH = 'FLUSH'
 
+DATA_STATE_TABLE = 'midonet_data_state'
+DATA_VERSIONS_TABLE = 'midonet_data_versions'
+TASK_STATE_TABLE = 'midonet_task_state'
+
 LOG = logging.getLogger(__name__)
 _LI = i18n._LI
 
 
-class TaskState(model_base.BASEV2):
-    __tablename__ = 'midonet_task_state'
+class DataState(model_base.BASEV2):
+    __tablename__ = DATA_STATE_TABLE
     id = sa.Column(sa.Integer(), primary_key=True)
     last_processed_id = sa.Column(sa.Integer(),
                                   sa.ForeignKey('midonet_tasks.id'))
     updated_at = sa.Column(sa.DateTime(), nullable=False)
+    active_version = sa.Column(sa.Integer())
+    readonly = sa.Column(sa.Boolean(), nullable=False)
 
 
 class Task(model_base.BASEV2):
@@ -88,7 +94,7 @@ def get_current_task_data(session):
 def get_task_list(session, show_unprocessed):
     tasks = session.query(Task)
     if show_unprocessed:
-        task_state = session.query(TaskState).one()
+        task_state = session.query(DataState).one()
         lp_id = task_state.last_processed_id
         if lp_id is not None:
             tasks = tasks.filter(Task.id > lp_id)
@@ -96,7 +102,7 @@ def get_task_list(session, show_unprocessed):
 
 
 def task_clean(session):
-    task_state = session.query(TaskState).one()
+    task_state = session.query(DataState).one()
     lp_id = task_state.last_processed_id
     task_state.update({'last_processed_id': None,
                        'updated_at': datetime.datetime.utcnow()})
