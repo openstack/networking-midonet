72a0b8c2958d5cc52e3409c5ba813d9661f4413d
Joe Mills <joe@midokura.com>
2015-03-31 05:07:44 +0000

    Add dynamic routing tables
    Adds the migration scripts to add the dynamic routing
    tables to the neutron database.
    
    Change-Id: I0081c68f4deeb11303926a850c9e54708cb0bc1f
    Partially-Implements: kilo/dynamic_routing
    Signed-off-by: Joe Mills <joe@midokura.com>

---
+++ midonet/neutron/db/migration/alembic_migration/versions/421564f630b1_add_dynamic_routing_tables.py
@@ -0,0 +1,70 @@
+# Copyright 2015 Midokura SARL
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""add dynamic routing tables
+
+Revision ID: 421564f630b1
+Revises: d8a5c672761
+Create Date: 2015-03-31 04:40:24.533580
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '421564f630b1'
+down_revision = 'd8a5c672761'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def create_midonet_routing_instances_table():
+    op.create_table(
+        'midonet_routing_instances',
+        sa.Column('id', sa.String(length=36), primary_key=True),
+        sa.Column('router_id', sa.String(length=36), nullable=False),
+        sa.Column('local_as', sa.Integer(), nullable=False),
+        sa.Column('protocol', sa.String(length=255), nullable=False),
+        sa.ForeignKeyConstraint(['router_id'], ['routers.id']))
+
+
+def create_midonet_routing_peers():
+    op.create_table(
+        'midonet_routing_peers',
+        sa.Column('id', sa.String(length=36), primary_key=True),
+        sa.Column('routing_instance_id', sa.String(length=36),
+                  nullable=False),
+        sa.Column('port_id', sa.String(length=36), nullable=False),
+        sa.Column('peer_as', sa.Integer(), nullable=False),
+        sa.Column('peer_address', sa.String(length=255), nullable=False),
+        sa.ForeignKeyConstraint(['routing_instance_id'],
+                                ['midonet_routing_instances.id']),
+        sa.ForeignKeyConstraint(['port_id'], ['ports.id']))
+
+
+def create_midonet_advertise_route():
+    op.create_table(
+        'midonet_advertise_route',
+        sa.Column('id', sa.String(length=36), primary_key=True),
+        sa.Column('routing_instance_id', sa.String(length=36),
+                  nullable=False),
+        sa.Column('destination', sa.String(length=36),
+                  nullable=False),
+        sa.ForeignKeyConstraint(['routing_instance_id'],
+                                ['midonet_routing_instances.id']))
+
+
+def upgrade():
+    create_midonet_routing_instances_table()
+    create_midonet_routing_peers()
+    create_midonet_advertise_route()
