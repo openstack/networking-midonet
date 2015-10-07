a67b69d6200f7326af9e4a590c367f49e8b0af57
YAMAMOTO Takashi <yamamoto@midokura.com>
2015-10-07 13:17:53 +0900

    Remove an unused decorator, retry_on_error
    Partial-Bug: #1503141
    Change-Id: Icb04c44fc30623fdbc9a4173d8b70838c8e788a1

--- midonet/neutron/common/util.py
+++ midonet/neutron/common/util.py
@@ -13,40 +13,9 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import time
-
 from neutron import i18n
 from oslo_log import log as logging
 
 
 LOG = logging.getLogger(__name__)
 _LW = i18n._LW
-
-
-def retry_on_error(attempts, delay, error_cls):
-    """Decorator for error handling retry logic
-
-    This decorator retries the function specified number of times with
-    specified delay between each attempt, for every exception thrown specified
-    in error_cls.  If case the retry fails in all attempts, the error_cls
-    exception object is thrown.
-
-    :param attempts: Number of retry attempts
-    :param delay: Delay in seconds between attempts
-    :param error_cls: The exception class that triggers a retry attempt
-    """
-    def internal_wrapper(func):
-        def retry(*args, **kwargs):
-            err = None
-            for i in range(attempts):
-                try:
-                    return func(*args, **kwargs)
-                except error_cls as exc:
-                    LOG.warn(_LW('Retrying because of error: %r'), exc)
-                    time.sleep(delay)
-                    err = exc
-            # err should always be set to a valid exception object
-            assert isinstance(err, error_cls)
-            raise err
-        return retry
-    return internal_wrapper
