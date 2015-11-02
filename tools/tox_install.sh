9e059a3f595f327bf6363ed7e4ceff7819a780d4
YAMAMOTO Takashi <yamamoto@midokura.com>
2015-11-02 12:59:42 +0900

    Stop doing any magic cloning of neutron during CI
    Adapted from neutron-lbaas change [1] done by Doug Wiegley.
    
    [1] Iee43c68b4cc954b9780de02625de7e053ebd27b3
    
    Change-Id: I75c9ad90c7c478feaca6643a4b4c3c59953dbb82
    (cherry picked from commit 18eb7b079f280e78dbc44c3ab673984fd4486df4)

---
+++ tools/tox_install.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+
+set -e
+
+DIR=$(dirname $0)
+${DIR}/tox_install_project.sh neutron $*
+${DIR}/tox_install_project.sh neutron-fwaas $*
+${DIR}/tox_install_project.sh neutron-lbaas $*
---
+++ tools/tox_install_project.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+# Many of neutron's repos suffer from the problem of depending on neutron,
+# but it not existing on pypi.
+
+# This wrapper for tox's package installer will use the existing package
+# if it exists, else use zuul-cloner if that program exists, else grab it
+# from neutron master via a hard-coded URL. That last case should only
+# happen with devs running unit tests locally.
+
+# From the tox.ini config page:
+# install_command=ARGV
+# default:
+# pip install {opts} {packages}
+
+PROJ=$1
+shift 1
+
+ZUUL_CLONER=/usr/zuul-env/bin/zuul-cloner
+neutron_installed=$(echo "import ${PROJ}" | python 2>/dev/null ; echo $?)
+
+set -e
+
+if [ $neutron_installed -eq 0 ]; then
+    echo "ALREADY INSTALLED" > /tmp/tox_install.txt
+    echo "Neutron already installed; using existing package"
+elif [ -x "$ZUUL_CLONER" ]; then
+    echo "ZUUL CLONER" > /tmp/tox_install.txt
+    cwd=$(/bin/pwd)
+    cd /tmp
+    $ZUUL_CLONER --cache-dir \
+        /opt/git \
+        git://git.openstack.org \
+        openstack/${PROJ}
+    cd openstack/${PROJ}
+    pip install -e .
+    cd "$cwd"
+else
+    echo "PIP HARDCODE" > /tmp/tox_install.txt
+    pip install -U -egit+https://git.openstack.org/openstack/${PROJ}#egg=${PROJ}
+fi
+
+pip install -U $*
+exit $?
