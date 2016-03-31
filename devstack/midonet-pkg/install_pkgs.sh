7e680ab7faece0decacbd6d8f30b29552ea709e4
YAMAMOTO Takashi <yamamoto@midokura.com>
2016-03-31 18:20:41 +0900

    devstack: Add an option to use MidoNet binary packges
    Only for v2 plugin for now.
    
    Note: uninstall_pkgs.sh is not currently used but useful for testing
    
    Closes-Bug: #1564283
    Change-Id: Ic7f1da9185148c41a367bd1fdca5650a1aec8da6

---
+++ devstack/midonet-pkg/install_pkgs.sh
@@ -0,0 +1,38 @@
+#! /bin/sh
+
+# Copyright (c) 2016 Midokura SARL
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+# Remove possible reminders from the previous devmido runs
+rm -rf \
+    /usr/local/bin/mn-conf \
+    /usr/local/bin/mm-ctl \
+    /usr/local/bin/mm-dpctl \
+    /usr/local/bin/mm-meter \
+    /usr/local/bin/mm-trace
+
+DEBIAN_FRONTEND=noninteractive
+export DEBIAN_FRONTEND
+
+apt-get install -y --no-install-recommends --no-install-suggests \
+    openjdk-8-jre-headless
+
+apt-get install -y --no-install-recommends --no-install-suggests \
+    zookeeperd \
+    dsc22 \
+    midonet-tools \
+    midonet-cluster \
+    midolman \
+    python-midonetclient
