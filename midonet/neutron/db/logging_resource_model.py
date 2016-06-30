bb7f5d0b96038333407800e06efb92622007b84a
YAMAMOTO Takashi <yamamoto@midokura.com>
2016-06-30 14:34:58 +0900

    logging_resource: Separate models from db mixins
    So that db migration can be run without neutron-fwaas installed.
    
    Note: v1 plugin doesn't support fwaas.
    
    Note: the FK to firewalls.id should be ok because the table is
    created by db migration of neutron. (not -fwaas)
    
    Closes-Bug: #1597604
    Change-Id: I9a58f38449543700042e353a384fde75951f9a2f

---
+++ midonet/neutron/db/logging_resource_model.py
@@ -0,0 +1,56 @@
+# Copyright (C) 2016 Midokura SARL.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import sqlalchemy as sa
+from sqlalchemy import orm
+
+from neutron.db import model_base
+
+LOGGING_RESOURCES = 'midonet_logging_resources'
+FIREWALL_LOGS = 'midonet_firewall_logs'
+
+
+class LoggingResource(model_base.BASEV2):
+    """Represents a logging resource."""
+
+    __tablename__ = LOGGING_RESOURCES
+    id = sa.Column(sa.String(36), primary_key=True)
+    name = sa.Column(sa.String(255))
+    description = sa.Column(sa.String(1024))
+    tenant_id = sa.Column(sa.String(length=255))
+    enabled = sa.Column(sa.Boolean, nullable=False)
+
+
+class FirewallLog(model_base.BASEV2):
+    """Represents a firewall log."""
+
+    __tablename__ = FIREWALL_LOGS
+
+    id = sa.Column(sa.String(36), primary_key=True)
+    logging_resource_id = sa.Column(sa.String(36),
+                          sa.ForeignKey('midonet_logging_resources.id',
+                          ondelete="CASCADE"),
+                          nullable=False)
+    tenant_id = sa.Column(sa.String(length=255))
+    description = sa.Column(sa.String(1024))
+    fw_event = sa.Column(sa.String(length=255), nullable=False)
+    firewall_id = sa.Column(sa.String(36),
+                  sa.ForeignKey('firewalls.id',
+                  ondelete="CASCADE"),
+                  nullable=False)
+    logging_resource = orm.relationship(
+        LoggingResource,
+        backref=orm.backref('firewall_logs', cascade='delete', lazy='joined'),
+        primaryjoin="LoggingResource.id==FirewallLog.logging_resource_id")
