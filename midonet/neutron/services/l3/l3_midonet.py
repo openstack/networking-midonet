c449036ea90381e7e0a0ab5d1f3a824a0b1efaa2
YAMAMOTO Takashi <yamamoto@midokura.com>
2016-08-24 10:27:13 +0900

    l3: Remove obsolete explicit subscription
    Recent neutron change [1] made it no-op.
    
    [1] Id5dd012ffd274314f7d1b39a28525893e0675500
    
    Change-Id: I93d1512ff852aa88784b4c8d778bd8e623990a93

--- midonet/neutron/services/l3/l3_midonet.py
+++ midonet/neutron/services/l3/l3_midonet.py
@@ -30,7 +30,6 @@ from neutron.callbacks import registry
 from neutron.callbacks import resources
 from neutron.db import common_db_mixin
 from neutron.db import extraroute_db
-from neutron.db import l3_db
 # Import l3_dvr_db to get the config options required for FWaaS
 from neutron.db import l3_dvr_db  # noqa
 from neutron.extensions import l3
@@ -61,7 +60,6 @@ class MidonetL3ServicePlugin(common_db_mixin.CommonDbMixin,
 
     def __init__(self):
         super(MidonetL3ServicePlugin, self).__init__()
-        l3_db.subscribe()
         self.__subscribe()
 
         # Instantiate MidoNet API client
