768ddc20928fc5f08e1a06c9ae4ac078bd03ab55
YAMAMOTO Takashi <yamamoto@midokura.com>
2016-11-29 19:38:17 +0900

    gate_hook: Add "full" variants of ml2 and v2
    At this point, they are identical with the non-full variants.
    The plan is to introduce new non-voting jobs for these and
    make the existing voting jobs minimum later.
    
    Change-Id: Ib64ed914a3242c27876ae2702079ad38c3d41204
    Partial-Bug: #1615668

--- devstack/ci/gate_hook.sh
+++ devstack/ci/gate_hook.sh
@@ -42,7 +42,34 @@ case $job in
         export DEVSTACK_LOCAL_CONFIG+=$'\n'"Q_SERVICE_PLUGIN_CLASSES=midonet.neutron.services.l3.l3_midonet.MidonetL3ServicePlugin"
         _ML2=False
         ;;
+    v2-full)
+        # Note the actual url here is somewhat irrelevant because it
+        # caches in nodepool, however make it a valid url for
+        # documentation purposes.
+        export DEVSTACK_LOCAL_CONFIG="enable_plugin networking-midonet git://git.openstack.org/openstack/networking-midonet"
+        export DEVSTACK_LOCAL_CONFIG+=$'\n'"Q_PLUGIN=midonet"
+        export DEVSTACK_LOCAL_CONFIG+=$'\n'"TEMPEST_RUN_VALIDATION=True"
+
+        # Enable MidoNet v2 architecture
+        export DEVSTACK_LOCAL_CONFIG+=$'\n'"MIDONET_PLUGIN=midonet.neutron.plugin_v2.MidonetPluginV2"
+        export DEVSTACK_LOCAL_CONFIG+=$'\n'"MIDONET_CLIENT=midonet.neutron.client.api.MidonetApiClient"
+        export DEVSTACK_LOCAL_CONFIG+=$'\n'"Q_SERVICE_PLUGIN_CLASSES=midonet.neutron.services.l3.l3_midonet.MidonetL3ServicePlugin"
+        _ML2=False
+        ;;
     ml2)
+        # Note the actual url here is somewhat irrelevant because it
+        # caches in nodepool, however make it a valid url for
+        # documentation purposes.
+        export DEVSTACK_LOCAL_CONFIG="enable_plugin networking-midonet git://git.openstack.org/openstack/networking-midonet"
+        export DEVSTACK_LOCAL_CONFIG+=$'\n'"Q_PLUGIN=ml2"
+        export DEVSTACK_LOCAL_CONFIG+=$'\n'"TEMPEST_RUN_VALIDATION=True"
+        export DEVSTACK_LOCAL_CONFIG+=$'\n'"Q_ML2_PLUGIN_MECHANISM_DRIVERS=midonet"
+        export DEVSTACK_LOCAL_CONFIG+=$'\n'"Q_ML2_PLUGIN_TYPE_DRIVERS=midonet,uplink"
+        export DEVSTACK_LOCAL_CONFIG+=$'\n'"Q_ML2_TENANT_NETWORK_TYPE=midonet"
+        export DEVSTACK_LOCAL_CONFIG+=$'\n'"ML2_L3_PLUGIN=midonet.neutron.services.l3.l3_midonet.MidonetL3ServicePlugin"
+        _ML2=True
+        ;;
+    ml2-full)
         # Note the actual url here is somewhat irrelevant because it
         # caches in nodepool, however make it a valid url for
         # documentation purposes.
