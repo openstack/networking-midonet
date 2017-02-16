895bdeb97b86d3e5a99ac787fcfb96a0cb47f447
YAMAMOTO Takashi <yamamoto@midokura.com>
2017-02-16 13:07:35 +0900

    sg_callback: Remove a hack for kilo
    The use of weakref has been removed from
    the callback registry long ago. [1]
    
    [1] Idcd0286fc4235af82901c8a17ea45bc758b62b37
    
    Closes-Bug: #1549591
    Change-Id: Ib0fa10bcd35ac419bbf2237d63974f965be91963

--- midonet/neutron/ml2/sg_callback.py
+++ midonet/neutron/ml2/sg_callback.py
@@ -92,29 +92,18 @@ class MidonetSecurityGroupsHandler(object):
                           {"sgr_id": sgr_id, "err": ex})
 
     def subscribe(self):
-        # REVISIT(Ryu): Keep an extra set of strong references to the callback
-        # methods so that they are not prematurely garbage collected.
-        # This hack is required in the Liberty branch (not stable/kilo) because
-        # currently the Liberty mechanism driver is expected to work with Kilo
-        # ML2 plugin for MidoNet, and in Kilo ML2, there is a bug in which
-        # these methods were referenced as weakrefs where a garbage collection
-        # could remove these callbacks if they were part of an object.
-
-        self._create_sg_f = self.create_security_group
-        self._update_sg_f = self.update_security_group
-        self._delete_sg_f = self.delete_security_group
-        self._create_sgr_f = self.create_security_group_rule
-        self._delete_sgr_f = self.delete_security_group_rule
-
         registry.subscribe(
-            self._create_sg_f, resources.SECURITY_GROUP, events.AFTER_CREATE)
+            self.create_security_group,
+            resources.SECURITY_GROUP, events.AFTER_CREATE)
         registry.subscribe(
-            self._update_sg_f, resources.SECURITY_GROUP, events.AFTER_UPDATE)
+            self.update_security_group,
+            resources.SECURITY_GROUP, events.AFTER_UPDATE)
         registry.subscribe(
-            self._delete_sg_f, resources.SECURITY_GROUP, events.AFTER_DELETE)
+            self.delete_security_group,
+            resources.SECURITY_GROUP, events.AFTER_DELETE)
         registry.subscribe(
-            self._create_sgr_f, resources.SECURITY_GROUP_RULE,
-            events.AFTER_CREATE)
+            self.create_security_group_rule,
+            resources.SECURITY_GROUP_RULE, events.AFTER_CREATE)
         registry.subscribe(
-            self._delete_sgr_f, resources.SECURITY_GROUP_RULE,
-            events.AFTER_DELETE)
+            self.delete_security_group_rule,
+            resources.SECURITY_GROUP_RULE, events.AFTER_DELETE)
