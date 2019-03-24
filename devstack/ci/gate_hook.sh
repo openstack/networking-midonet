e979f8c7c4bd2bea86bbb0cb43c10f6957604cb9
Ian Wienand <iwienand@redhat.com>
2019-03-24 20:34:26 +0000

    Replace openstack.org git:// URLs with https://
    This is a mechanically generated change to replace openstack.org
    git:// URLs with https:// equivalents.
    
    This is in aid of a planned future move of the git hosting
    infrastructure to a self-hosted instance of gitea (https://gitea.io),
    which does not support the git wire protocol at this stage.
    
    This update should result in no functional change.
    
    For more information see the thread at
    
     http://lists.openstack.org/pipermail/openstack-discuss/2019-March/003825.html
    
    Change-Id: I2885901c6f352b2aeb01d1fd0819a04979e612f4

--- devstack/ci/gate_hook.sh
+++ devstack/ci/gate_hook.sh
@@ -21,7 +21,7 @@ case $job in
         # Note the actual url here is somewhat irrelevant because it
         # caches in nodepool, however make it a valid url for
         # documentation purposes.
-        export DEVSTACK_LOCAL_CONFIG="enable_plugin networking-midonet git://git.openstack.org/openstack/networking-midonet"
+        export DEVSTACK_LOCAL_CONFIG="enable_plugin networking-midonet https://git.openstack.org/openstack/networking-midonet"
         export DEVSTACK_LOCAL_CONFIG+=$'\n'"Q_PLUGIN=midonet"
         export DEVSTACK_LOCAL_CONFIG+=$'\n'"TEMPEST_RUN_VALIDATION=True"
 
@@ -41,7 +41,7 @@ case $job in
         # Note the actual url here is somewhat irrelevant because it
         # caches in nodepool, however make it a valid url for
         # documentation purposes.
-        export DEVSTACK_LOCAL_CONFIG="enable_plugin networking-midonet git://git.openstack.org/openstack/networking-midonet"
+        export DEVSTACK_LOCAL_CONFIG="enable_plugin networking-midonet https://git.openstack.org/openstack/networking-midonet"
         export DEVSTACK_LOCAL_CONFIG+=$'\n'"Q_PLUGIN=midonet"
         export DEVSTACK_LOCAL_CONFIG+=$'\n'"TEMPEST_RUN_VALIDATION=True"
 
@@ -57,7 +57,7 @@ case $job in
         # Note the actual url here is somewhat irrelevant because it
         # caches in nodepool, however make it a valid url for
         # documentation purposes.
-        export DEVSTACK_LOCAL_CONFIG="enable_plugin networking-midonet git://git.openstack.org/openstack/networking-midonet"
+        export DEVSTACK_LOCAL_CONFIG="enable_plugin networking-midonet https://git.openstack.org/openstack/networking-midonet"
         export DEVSTACK_LOCAL_CONFIG+=$'\n'"Q_PLUGIN=ml2"
         export DEVSTACK_LOCAL_CONFIG+=$'\n'"TEMPEST_RUN_VALIDATION=True"
         export DEVSTACK_LOCAL_CONFIG+=$'\n'"Q_ML2_PLUGIN_MECHANISM_DRIVERS=midonet"
@@ -72,8 +72,8 @@ case $job in
         # Note the actual url here is somewhat irrelevant because it
         # caches in nodepool, however make it a valid url for
         # documentation purposes.
-        export DEVSTACK_LOCAL_CONFIG="enable_plugin networking-midonet git://git.openstack.org/openstack/networking-midonet"
-        export DEVSTACK_LOCAL_CONFIG+=$'\n'"enable_plugin rally git://git.openstack.org/openstack/rally"
+        export DEVSTACK_LOCAL_CONFIG="enable_plugin networking-midonet https://git.openstack.org/openstack/networking-midonet"
+        export DEVSTACK_LOCAL_CONFIG+=$'\n'"enable_plugin rally https://git.openstack.org/openstack/rally"
         export DEVSTACK_LOCAL_CONFIG+=$'\n'"Q_PLUGIN=midonet"
         export DEVSTACK_LOCAL_CONFIG+=$'\n'"TEMPEST_RUN_VALIDATION=True"
 
